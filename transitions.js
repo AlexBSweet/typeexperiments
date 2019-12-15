7f9bb483d58e0b78d730c7bc79fc80b435a32158
Alex Sweet <alexsweet@Alexs-MacBook-Pro-2.local>
2019-12-14 18:38:27 -0600

    test

--- transitions.js
+++ transitions.js
@@ -9,61 +9,8 @@ bodyTag.appendChild(wiper)
 
 
 
-const containerOne = document.querySelector('div.container-one')
-const containerTwoSpans = document.querySelectorAll('div.container-two span')
-const containerThreeSpans = document.querySelectorAll('div.container-three span')
-
-const sections = document.querySelectorAll('section')
-
-
-
-
-const runScripts = function (){
-    const observer = new IntersectionObserver(
-        entries => {
-          entries.forEach(entry => {
-    
-    
-            if (entry.intersectionRatio >= 0.1) {
-              entry.target.classList.add('in-view')
-              console.log('hello')
-            } else {
-              entry.target.classList.remove('in-view')
-            }
-        })
-      },
-      {
-        threshold: [0, 0.1, 1]
-      }
-    )
-    
-        console.log(sections)
-    
-    // sections.forEach(section=>{
-    //     observer.observe(section)
-    // })
-    containerTwoSpans.forEach(span=>{
-        observer.observe(span)
-    })
-    containerThreeSpans.forEach(span=>{
-        observer.observe(span)
-    })
-}
 
 runScripts()
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 barba.init({
     debug:true,
